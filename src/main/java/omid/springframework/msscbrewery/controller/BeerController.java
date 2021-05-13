6747fb81a2ac17e536de67f952fac743e038298f
omid-joukar <joukaromid6@gmail.com>
2021-05-13 12:01:41 +0200

    added test.

--- src/main/java/omid/springframework/msscbrewery/controller/BeerController.java
+++ src/main/java/omid/springframework/msscbrewery/controller/BeerController.java
@@ -24,14 +24,14 @@ public class BeerController {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location","/api/v1/beer/" + savedDto.getId().toString());
         return new ResponseEntity(headers,HttpStatus.CREATED);
     }
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,BeerDto beerDto){
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
         beerService.updateBeer(beerId,beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
