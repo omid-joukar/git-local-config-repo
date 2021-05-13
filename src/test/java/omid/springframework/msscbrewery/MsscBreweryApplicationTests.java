f0260cbdaaeb2939c622b036d8826d18753b80df
omid-joukar <joukaromid6@gmail.com>
2021-05-13 10:29:30 +0200

    init project.

---
+++ src/test/java/omid/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package omid.springframework.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
