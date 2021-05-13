042ab2c4a332926249c462a956fde08d9cbeb53d
omid-joukar <joukaromid6@gmail.com>
2021-05-13 12:12:08 +0200

    added customer Http requests .

--- src/main/java/omid/springframework/msscbrewery/services/CustomerServieImpl.java
+++ src/main/java/omid/springframework/msscbrewery/services/CustomerServieImpl.java
@@ -1,17 +1,37 @@
 package omid.springframework.msscbrewery.services;
 
+import lombok.extern.slf4j.Slf4j;
 import omid.springframework.msscbrewery.model.CustomerDto;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class CustomerServieImpl implements CustomerService{
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder()
                 .id(UUID.randomUUID())
-                .name("Joe Back")
+                .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl
+        log.debug("Updating....");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting.... ");
     }
 }
